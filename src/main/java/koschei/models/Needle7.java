6b1d369c86907425e008d44f680984742b1a14a4
Arseny <senyanyash64@gmail.com>
2023-01-03 22:24:41 +0300

    v1.1

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,13 +6,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-    private Deth8 deth;
-
-    @Autowired
-    public Needle7(Deth8 deth) {
-        this.deth = deth;
-    }
-
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( ";
